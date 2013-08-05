f47f99d0d463fea549ae2580bdc59e9e9d17cfd2
Ryan Buckley <arebuckley@gmail.com>
2013-08-04 20:00:21 -1000

    settings for email

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -46,4 +46,8 @@ HanaMech::Application.configure do
 
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
+  config.action_mailer.default_url_options = {
+      :host => 'hanamech.com',
+      :protocol => 'http'
+  }
 end
