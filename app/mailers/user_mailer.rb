5a82fbb949df7e3c8e7cc0531b9fc1d55854460e
Ryan Buckley <arebuckley@gmail.com>
2013-08-04 20:39:18 -1000

    add name to from email

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -3,7 +3,7 @@ class UserMailer < ActionMailer::Base
     @user = User.find(user)
     @plain_password = tmp_pass
     mail :to => @user.email,
-         :from => 'info@hanamech.com',
+         :from => 'Hana Mechanical <info@hanamech.com>',
          :subject => "Hana Mechanical - Account Created"
   end
 end
