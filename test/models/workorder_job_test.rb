1425595b3f1d33d89caeb5be1a39b3835a8fb7af
Ryan Buckley <arebuckley@gmail.com>
2015-10-21 15:27:08 -0700

    remove test dir

--- test/models/workorder_job_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class WorkorderJobTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
--- test/models/workorder_part_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class WorkorderPartTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
